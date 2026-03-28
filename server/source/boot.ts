15ed82542825b10ca0bfe99a8658f0e0201a80e0
roney ruiz <roneyruizrojas@gmail.com>
2026-03-27 21:33:41 -0600

    feat(server): create empty users plugin

--- server/source/boot.ts
+++ server/source/boot.ts
@@ -1,8 +1,10 @@
 import { Elysia } from "elysia";
 import { HealthPlugin } from "@plugins/health.plugin";
+import { UsersPlugin } from "@features/users/plugin";
 
 const app = new Elysia({ prefix: "/api" })
   .use(HealthPlugin)
+  .use(UsersPlugin)
   .listen(process.env.PORT!);
 
 const url = `http://${app.server?.hostname}:${app.server?.port}`;
