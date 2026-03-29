5d93089565911181a0e31505d14d50f63e042ce8
roney ruiz <roneyruizrojas@gmail.com>
2026-03-29 17:17:45 -0600

    chore(server): purge all users module temporary

--- server/source/features/users/plugin.ts
+++
@@ -1,9 +0,0 @@
-import { Elysia, type ElysiaConfig } from "elysia";
-import { ProfilePlugin } from "./plugins/profile.plugin";
-
-const config: ElysiaConfig<"/users"> = {
-  prefix: "/users", name: "users.plugin"
-};
-
-export const UsersPlugin = new Elysia(config)
-  .use(ProfilePlugin);
