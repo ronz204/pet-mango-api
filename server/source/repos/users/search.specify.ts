9fa455828c95331c290dedc723d32a7a5a249f60
roney ruiz <roneyruizrojas@gmail.com>
2026-03-29 17:18:21 -0600

    feat(server): create user repos to data access

---
+++ server/source/repos/users/search.specify.ts
@@ -0,0 +1,22 @@
+import { Specify } from "@contracts/specify.contract";
+import type { UserFindFirstArgs } from "@prisma/models";
+
+interface SpecifyArgs {
+  id?: number;
+  name?: string;
+  email?: string;
+};
+
+export class SearchUserSpecify extends Specify {
+  constructor(private readonly args: SpecifyArgs) {super()};
+
+  public override toQuery() {
+    return {
+      where: {
+        id: this.args.id,
+        name: this.args.name,
+        email: this.args.email,
+      },
+    } as const satisfies UserFindFirstArgs;
+  };
+};
