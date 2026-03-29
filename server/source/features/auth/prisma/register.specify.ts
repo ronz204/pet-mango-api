db45fe120d0ac453d6ea1953304d0e018da30707
roney ruiz <roneyruizrojas@gmail.com>
2026-03-28 21:54:35 -0600

    feat(server): complete register specify query

---
+++ server/source/features/auth/prisma/register.specify.ts
@@ -0,0 +1,22 @@
+import { Specify } from "@contracts/specify.contract";
+import type { UserCreateArgs } from "@prisma/models";
+
+interface SpecifyArgs {
+  name: string;
+  email: string;
+  password: string;
+};
+
+export class RegisterSpecify extends Specify {
+  constructor(private readonly args: SpecifyArgs) {super()};
+
+  public override toQuery() {
+    return {
+      data: {
+        name: this.args.name,
+        email: this.args.email,
+        password: this.args.password,
+      },
+    } as const satisfies UserCreateArgs;
+  };
+};
