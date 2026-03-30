f1751ab61b3896ef02f99619dce72f865a8359ae
roney ruiz <roneyruizrojas@gmail.com>
2026-03-29 21:06:43 -0600

    chore(server): purge errors temporary

--- server/source/plugins/errors.plugin.ts
+++ server/source/plugins/errors.plugin.ts
@@ -1,10 +1,8 @@
 import { Elysia } from "elysia";
-import { MangoError } from "@contracts/errors.contract";
-import { ConflictError } from "@shared/errors/conflict.error";
 
 export const ErrorsPlugin = new Elysia()
   .error({
-    ConflictError,
+
   })
   
   .onError(({ error }) => {
--- server/source/shared/errors/conflict.error.ts
+++
@@ -1,8 +0,0 @@
-import type { ErrorArgs } from "@contracts/errors.contract";
-import { MangoError } from "@contracts/errors.contract";
-
-export class ConflictError extends MangoError {
-  constructor(args: ErrorArgs) {
-    super(409, args);
-  };
-};
