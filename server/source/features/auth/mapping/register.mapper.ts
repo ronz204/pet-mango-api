b97d9cf33120eb244c19fc84fb3db0a1a1d8465a
roney ruiz <roneyruizrojas@gmail.com>
2026-03-31 11:14:01 -0600

    chore(server): purge all auth feature

--- server/source/features/auth/mapping/register.mapper.ts
+++
@@ -1,8 +0,0 @@
-import type { User } from "@prisma/client";
-import type { RegisterUserPayload } from "../schemas/register.schema";
-
-export class RegisterUserMapper {
-  public static toResponse(data: User): RegisterUserPayload {
-    return { id: data.id };
-  };
-};
