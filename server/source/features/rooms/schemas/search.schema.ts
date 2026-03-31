3ef88e823ac38270eba5b2207f86a211bb6b1bbe
roney ruiz <roneyruizrojas@gmail.com>
2026-03-30 20:22:52 -0600

    feat(server): complete search public rooms schema and mapper

---
+++ server/source/features/rooms/schemas/search.schema.ts
@@ -0,0 +1,23 @@
+import { t, type Static } from "elysia";
+
+const RoomDto = t.Object({
+  id: t.Number(),
+  name: t.String(),
+  members: t.Number(),
+});
+
+export const SearchRoomsQuery = t.Object({
+  name: t.Optional(t.String({ minLength: 1, maxLength: 50 })),
+});
+
+export const SearchRoomsRequest = t.Object({
+  query: SearchRoomsQuery,
+  user: t.Number(),
+});
+
+export const SearchRoomsResponse = t.Object({
+  rooms: t.Array(RoomDto),
+});
+
+export type SearchRoomsRequest = Static<typeof SearchRoomsRequest>;
+export type SearchRoomsResponse = Static<typeof SearchRoomsResponse>;
