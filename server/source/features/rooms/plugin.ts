9596e39afe9ec4eaa81d81d4f7e2c872eeccd55f
roney ruiz <roneyruizrojas@gmail.com>
2026-03-30 20:23:10 -0600

    feat(server): complete search public rooms handler and plugin

--- server/source/features/rooms/plugin.ts
+++ server/source/features/rooms/plugin.ts
@@ -1,9 +1,11 @@
 import { Elysia, type ElysiaConfig } from "elysia";
 import { CreateRoomPlugin } from "./plugins/create.plugin";
+import { SearchRoomsPlugin } from "./plugins/search.plugin";
 
 const config: ElysiaConfig<"/rooms"> = {
   prefix: "/rooms", name: "rooms.plugin"
 };
 
 export const RoomsPlugin = new Elysia(config)
-  .use(CreateRoomPlugin);
+  .use(CreateRoomPlugin)
+  .use(SearchRoomsPlugin);
---
+++ server/source/features/rooms/plugins/search.plugin.ts
@@ -0,0 +1,27 @@
+import { Elysia } from "elysia";
+import { TokenPlugin } from "@auth/token.plugin";
+import { PrismaPlugin } from "@database/prisma.plugin";
+import { SearchRoomsQuery } from "../schemas/search.schema";
+import { SearchRoomsResponse } from "../schemas/search.schema";
+import { SearchRoomsHandler } from "../handlers/search.handler";
+
+const name: string = "search.rooms.plugin";
+
+export const SearchRoomsPlugin = new Elysia({ name })
+  .use(PrismaPlugin)
+  .use(TokenPlugin)
+
+  .derive(({ prisma }) => ({
+    searchH: new SearchRoomsHandler(prisma),
+  }))
+
+  .get("/search", async ({ query, status, user, searchH }) => {
+    const response = await searchH.handle({ query, user });
+    return status(200, response);
+  }, {
+    auth: true,
+    query: SearchRoomsQuery,
+    response: {
+      200: SearchRoomsResponse
+    },
+  });
