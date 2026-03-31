2a08f93a8700dedfa31fa59fbc6a03f47e4c12b6
roney ruiz <roneyruizrojas@gmail.com>
2026-03-30 21:40:29 -0600

    feat(server): create details by room specify

---
+++ server/source/dal/rooms/details.specify.ts
@@ -0,0 +1,23 @@
+import { Specify } from "@contracts/specify.contract";
+import type { RoomFindFirstArgs } from "@prisma/models";
+
+interface SpecifyArgs {
+  roomId: number;
+};
+
+export class RoomDetailsSpecify extends Specify {
+  constructor(private readonly args: SpecifyArgs) {super()};
+
+  public override toQuery() {
+    return {
+      where: { id: this.args.roomId },
+      include: {
+        members: {
+          include: {
+            user: true
+          },
+        },
+      },
+    } as const satisfies RoomFindFirstArgs;
+  };
+};
