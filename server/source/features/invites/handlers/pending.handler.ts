fef5c4b25348fb05be610ae8c9fffe9ee7234ce9
roney ruiz <roneyruizrojas@gmail.com>
2026-03-31 20:05:36 -0600

    chore(server): format pending components

--- server/source/features/invites/handlers/pending.handler.ts
+++ server/source/features/invites/handlers/pending.handler.ts
@@ -7,7 +7,7 @@ import type { PendingInvitesRequest as Request } from "../schemas/pending.schema
 import type { PendingInvitesResponse as Response } from "../schemas/pending.schema";
 
 export class PendingInvitesHandler implements Handler<Request, Response> {
-  constructor(private readonly prisma: PrismaClient) {}
+  constructor(private readonly prisma: PrismaClient) {};
 
   public async handle(request: Request): Promise<Response> {
     const pendingQuery = new PendingInvitesSpecify({
@@ -16,5 +16,5 @@ export class PendingInvitesHandler implements Handler<Request, Response> {
 
     const invitations = await this.prisma.invitation.findMany(pendingQuery);
     return PendingInvitesMapper.toResponse({ data: invitations });
-  }
+  };
 };
