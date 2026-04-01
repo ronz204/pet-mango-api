8feb7ddb075ce0416df51e473de93ecc0571d1f7
roney ruiz <roneyruizrojas@gmail.com>
2026-03-31 19:48:05 -0600

    deps(server): install openapi and config

--- server/source/boot.ts
+++ server/source/boot.ts
@@ -1,10 +1,12 @@
 import { Elysia } from "elysia";
+import { openapi } from "@elysiajs/openapi";
 import { UsersPlugin } from "@features/users/plugin";
 import { RoomsPlugin } from "@features/rooms/plugin";
 import { HealthPlugin } from "@plugins/health.plugin";
 import { AuthPlugin } from "@features/identity/plugin";
 
 const app = new Elysia({ prefix: "/api" })
+  .use(openapi())
   .use(HealthPlugin)
   .use(AuthPlugin)
   .use(UsersPlugin)
