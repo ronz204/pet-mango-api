e74c04f4c245c24a12b2196f112eb131cdad717c
roney ruiz <roneyruizrojas@gmail.com>
2026-03-31 20:04:36 -0600

    feat(server): complete read pending invitations schema and mapper

---
+++ server/source/features/invites/mapping/pending.mapper.ts
@@ -0,0 +1,22 @@
+import type { InvitationGetPayload } from "@prisma/models";
+import { PendingInvitesSpecify } from "@dal/invites/pending.specify";
+import type { PendingInvitesResponse } from "../schemas/pending.schema";
+
+type PendingInvitesQuery = ReturnType<PendingInvitesSpecify["toQuery"]>;
+
+interface MapperArgs {
+  data: InvitationGetPayload<PendingInvitesQuery>[];
+}
+
+export class PendingInvitesMapper {
+  public static toResponse({ data }: MapperArgs): PendingInvitesResponse {
+    return {
+      invitations: data.map((invitation) => ({
+        id: invitation.id,
+        roomId: invitation.roomId,
+        roomName: invitation.room.name,
+        createdAt: invitation.createdAt,
+      })),
+    };
+  }
+}
