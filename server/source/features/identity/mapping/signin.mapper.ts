ea7472eff4cc0412003b42b6ee2038d65250e673
roney ruiz <roneyruizrojas@gmail.com>
2026-04-02 17:12:37 -0600

    chore(server): IMPORTANT, drop all for refactor

--- server/source/features/identity/mapping/signin.mapper.ts
+++
@@ -1,8 +0,0 @@
-import type { User } from "@prisma/client";
-import { SignInPayload } from "../schemas/signin.schema";
-
-export class SignInMapper {
-  public static toResponse(data: User): SignInPayload {
-    return { id: data.id };
-  };
-};
--- server/source/features/identity/mapping/signup.mapper.ts
+++
@@ -1,8 +0,0 @@
-import type { User } from "@prisma/client";
-import type { SignUpPayload } from "../schemas/signup.schema";
-
-export class SignUpMapper {
-  public static toResponse(data: User): SignUpPayload {
-    return { id: data.id };
-  };
-};
