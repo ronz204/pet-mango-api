e13f44188351dbb7f0475ed945310980227c4328
roney ruiz <roneyruizrojas@gmail.com>
2026-04-17 22:36:56 -0600

    feat: complete get messages by room feature

--- source/boot.ts
+++ source/boot.ts
@@ -4,11 +4,14 @@ import { ErrorsPlugin } from "@plugins/errors.plugin";
 import { HealthPlugin } from "@plugins/health.plugin";
 import { ScalarPlugin } from "@plugins/scalar.plugin";
 
+import { RoomsPlugin } from "@features/rooms/rooms.plugin";
+
 export const app = new Elysia({ prefix: "/api" })
   .use(CorsPlugin)
   .use(ScalarPlugin)
   .use(HealthPlugin)
   .use(ErrorsPlugin)
+  .use(RoomsPlugin)
   .listen(3000);
 
 const url = `http://${app.server?.hostname}:${app.server?.port}`;
