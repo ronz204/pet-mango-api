d9501ee771431d8b1bbe757e6c40a08642ff5e2a
roney ruiz <roneyruizrojas@gmail.com>
2026-04-17 18:44:55 -0600

    refac: move read all members to member dao

--- source/core/dal/member/member.dao.ts
+++ source/core/dal/member/member.dao.ts
@@ -5,6 +5,7 @@ import type { Member } from "@prisma/client";
 import { Obtain } from "./queries/obtain.query";
 import { Create } from "./queries/create.query";
 import { Disable } from "./queries/disable.query";
+import { ReadAll } from "./queries/readall.query";
 
 export class MemberDao implements IMemberDao {
   constructor(private prisma: PrismaClient) {};
@@ -19,5 +20,9 @@ export class MemberDao implements IMemberDao {
 
   public async obtain(args: Obtain.Args): Promise<Member | null> {
     return await this.prisma.member.findFirst(Obtain.query(args));
+  };
+
+  public async readAll(args: ReadAll.Args): Promise<ReadAll.Result[]> {
+    return await this.prisma.member.findMany(ReadAll.query(args));
   };
 };
--- source/core/dal/member/member.idao.ts
+++ source/core/dal/member/member.idao.ts
@@ -3,9 +3,11 @@ import type { Member } from "@prisma/client";
 import { Obtain } from "./queries/obtain.query";
 import { Create } from "./queries/create.query";
 import { Disable } from "./queries/disable.query";
+import { ReadAll } from "./queries/readall.query";
 
 export interface IMemberDao {
   create(args: Create.Args): Promise<Member>;
   disable(args: Disable.Args): Promise<Member>;
   obtain(args: Obtain.Args): Promise<Member | null>;
+  readAll(args: ReadAll.Args): Promise<ReadAll.Result[]>;
 };
