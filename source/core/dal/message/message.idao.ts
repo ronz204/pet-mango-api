b420e4255b91c693ab7000cc4a1d26b24355af5e
roney ruiz <roneyruizrojas@gmail.com>
2026-05-01 23:08:56 -0600

    chore: IMPORTANT, purge all db queries and cache

--- source/core/dal/message/message.idao.ts
+++
@@ -1,7 +0,0 @@
-import { Create } from "./queries/create.query";
-import { ReadAll } from "./queries/readall.query";
-
-export interface IMessageDao {
-  read(args: ReadAll.Args): Promise<ReadAll.Result[]>;
-  create(args: Create.Args): Promise<Create.Result>;
-};
--- source/core/dal/users/user.dto.ts
+++
@@ -1,9 +0,0 @@
-import { t, type Static } from "elysia";
-
-export const UserDto = t.Object({
-  id: t.Number(),
-  name: t.String(),
-  email: t.String(),
-});
-
-export type UserDto = Static<typeof UserDto>;
