e383925876da27e3a96f83b2e48d8ce6393c4d67
roney ruiz <roneyruizrojas@gmail.com>
2026-05-01 23:28:58 -0600

    refac: use pg config for prisma plugin

--- source/database/prisma.plugin.ts
+++ source/database/prisma.plugin.ts
@@ -1,11 +1,15 @@
 import { Elysia } from "elysia";
 import { PrismaPg } from "@prisma/adapter-pg";
 import { PrismaClient } from "@prisma/client";
+import { PgConfig } from "@configs/postgres.config";
 
 const name: string = "prisma.plugin";
-
-const url = process.env.POSTGRES_URL ?? "";
-const adapter = new PrismaPg({ connectionString: url });
+const url = PgConfig.POSTGRES_URL;
 
 export const PrismaPlugin = new Elysia({ name })
-  .decorate("prisma", new PrismaClient({ adapter }));
+  .decorate(() => {
+    const adapter = new PrismaPg({ connectionString: url });
+    const prisma = new PrismaClient({ adapter });
+
+    return { prisma };
+  });
