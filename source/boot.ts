6a4c0322bd0b5c009e2367731f7d168f29190da7
roney ruiz <roneyruizrojas@gmail.com>
2026-05-01 23:08:26 -0600

    chore: IMPORTANT, purge all features

--- source/boot.ts
+++ source/boot.ts
@@ -1,27 +1,12 @@
 import { Elysia } from "elysia";
 import { CorsPlugin } from "@plugins/cors.plugin";
-import { ErrorsPlugin } from "@plugins/errors.plugin";
 import { HealthPlugin } from "@plugins/health.plugin";
 import { ScalarPlugin } from "@plugins/scalar.plugin";
-
-import { RoomsPlugin } from "@features/rooms/rooms.plugin";
-import { TypingPlugin } from "@features/typing/typing.plugin";
-import { ProfilePlugin } from "@features/profile/profile.plugin";
-import { IdentityPlugin } from "@features/identity/identity.plugin";
-import { PresencePlugin } from "@features/presence/presence.plugin";
-import { InvitationsPlugin } from "@features/invitation/invitation.plugin";
 
 export const app = new Elysia({ prefix: "/api" })
   .use(CorsPlugin)
   .use(ScalarPlugin)
   .use(HealthPlugin)
-  .use(ErrorsPlugin)
-  .use(IdentityPlugin)
-  .use(ProfilePlugin)
-  .use(RoomsPlugin)
-  .use(InvitationsPlugin)
-  .use(PresencePlugin)
-  .use(TypingPlugin)
   .listen(3000);
 
 const url = `http://${app.server?.hostname}:${app.server?.port}`;
--- source/features/profile/get-profile-info/get-profile-info.schema.ts
+++
@@ -1,7 +0,0 @@
-import { t, type Static } from "elysia";
-
-export const GetProfileInfoRequest = t.Object({
-  userId: t.Number(),
-});
-
-export type GetProfileInfoRequest = Static<typeof GetProfileInfoRequest>;
